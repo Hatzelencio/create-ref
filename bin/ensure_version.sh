4ad6eb1f45dd82bb892ef4a2f0d590f0797eba47
Hatzel Rentería <developerhatzel@gmail.com>
2020-06-12 19:21:06 -0500

    [SET] shebang in ensure version script

--- bin/ensure_version.sh
+++ bin/ensure_version.sh
@@ -1,3 +1,6 @@
-#!/bin/sh
+#!/bin/bash
 _=$(grep Version README.md | awk '{print "git rev-parse refs/tags/"$3" > /dev/null 2>&1;"}' | sh)
-if [ "$(echo $?)" == 0 ]; then exit 1; fi;
+if [ "$(echo $?)" == 0 ]
+then
+	exit 1
+fi
